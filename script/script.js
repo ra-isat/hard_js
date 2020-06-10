bbe25eaba049228b1861e49418feb32ab4124c55
Raisat Sirazhudinova <raisat13666@gmail.com>
2020-06-10 10:07:02 +0300

    Забыла убрать пробелы. Исправила

--- script/script.js
+++ script/script.js
@@ -4,9 +4,9 @@ function getCutString(str) {
     if (typeof(str) !== "string") {
         alert("Мы так не договаривались");
     } else {
-        console.log(str.substring(0, 31) + '...');
+        console.log(str.trim().substring(0, 31) + '...');
     }
 }
-let x = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nam corrupti fugiat eaque facilis quasi maiores, dolor, quos quisquam voluptatibus tempora dignissimos eius esse, voluptate consectetur non ipsum minima omnis?";
+let x = "      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nam corrupti fugiat eaque facilis quasi maiores, dolor, quos quisquam voluptatibus tempora dignissimos eius esse, voluptate consectetur non ipsum minima omnis?";
 // x = 1;
 getCutString(x);
