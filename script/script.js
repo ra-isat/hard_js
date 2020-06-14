4ffac6f47df00c95eced1723d4317e53491425fd
Raisat Sirazhudinova <raisat13666@gmail.com>
2020-06-14 09:42:34 +0300

    hard05

--- script/script.js
+++ script/script.js
@@ -1,12 +1,32 @@
 "use strict";
 
-function getCutString(str) {
-    if (typeof(str) !== "string") {
-        alert("Мы так не договаривались");
-    } else {
-        console.log(str.trim().substring(0, 31) + '...');
+// первый способ
+
+let arr = ["2315", "904286", "45672", "42424", "00992", "22222", "55123"];
+
+let nums = arr.filter(function (item) {
+    if (item.charAt(0) === '2' || item.charAt(0) === '4') {
+        console.log(item);
+    }
+});
+
+// второй способ
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
+        console.log(arr[i]);
     }
 }
-let x = "      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nam corrupti fugiat eaque facilis quasi maiores, dolor, quos quisquam voluptatibus tempora dignissimos eius esse, voluptate consectetur non ipsum minima omnis?";
-// x = 1;
-getCutString(x);
+
+// простые числа
+
+for (let i = 2;  i <= 100; i++) {
+  let count = 0;
+  for (let j = 2; j <= i; j++) {
+    if (i % j !== 0) {continue;}
+    count += 1;
+  }
+  
+  if (count === 1) {console.log(i);}  
+  
+}
+
